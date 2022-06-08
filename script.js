f8035f6202d045459d8a095fc5eff0ae044cb335
Ridho Rakhaman <91518402+bapongka@users.noreply.github.com>
2022-06-08 22:51:46 +0700

    Almost Finish
    -Add function toggle change theme
    -Add theme 2 & theme 3

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 const numbers = document.querySelectorAll(".number");
-const operators = document.querySelectorAll(".plus,.minus,.multiply,.divide");
+const operators = document.querySelectorAll(".tc");
 const btn_bs = document.querySelector(".backspace");
 const btn_c = document.querySelector(".clear");
 const btn_e = document.querySelector(".equal");
 const display1 = document.querySelector(".num1");
 const display2 = document.querySelector(".num2");
-const toggle_all = document.querySelectorAll(".toggle");
+const toggle_all = document.querySelectorAll(".toggle_input");
 const body = document.querySelector("body");
 let operator = "";
 let result = "";
 let num1 = "";
 let num2 = "";
+
+// Toggle Change Theme
+toggle_all.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const check = e.currentTarget.innerText;
+    if (check === "2" || e.currentTarget.id === "two") {
+      body.classList.add("theme2");
+      body.classList.remove("theme3");
+    } else if (check === "3" || e.currentTarget.id === "tree") {
+      body.classList.add("theme3");
+      body.classList.remove("theme2");
+    } else {
+      body.classList.remove("theme2");
+      body.classList.remove("theme3");
+    }
+  });
+});
 
 // Display the numbers on the screen
 numbers.forEach((btn) => {
@@ -21,7 +38,7 @@ numbers.forEach((btn) => {
   });
 });
 
-// Display the operator
+// Display & Function the operator
 operators.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     operator = e.currentTarget.innerText;
@@ -52,13 +69,14 @@ function operations() {
     result = num1 + parseFloat(display2.innerText);
   } else if (operator === "-") {
     result = num1 - parseFloat(display2.innerText);
-  } else if (operator === "x") {
+  } else if (operator === "*") {
     result = num1 * parseFloat(display2.innerText);
   } else if (operator === "/") {
     result = num1 / parseFloat(display2.innerText);
   }
 }
 
+// Button =
 btn_e.addEventListener("click", () => {
   operations();
   if (!display2.innerText) {
@@ -70,10 +88,12 @@ btn_e.addEventListener("click", () => {
   }
 });
 
+// Button BackSpace
 btn_bs.addEventListener("click", () => {
   display2.innerText = display2.innerText.slice(0, -1);
 });
 
+// Button Clear
 btn_c.addEventListener("click", () => {
   display1.innerText = "";
   display2.innerText = "";
